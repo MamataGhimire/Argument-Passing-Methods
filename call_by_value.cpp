c6e87ae44feef410cd0f50061d07ea8cbd8f174a
Mamata Ghimire <89641735+MamataGhimire@users.noreply.github.com>
2024-12-02 20:02:31 +0545

    A copy of the value is passed to the function

---
+++ call_by_value.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void valueChange(int a,int b);
+
+int main()
+{
+int a=4,b=3;
+cout<<"value of a and b are"<<" "<<a<<" "<<b<<endl;
+
+valueChange(a,b);
+
+cout<<"value of a and b are"<<" "<<a<<" "<<b;
+
+    return 0;
+}
+void valueChange(int a,int b)
+{
+    int c=a;
+    a=a+b;
+    b=c-b;
+}
